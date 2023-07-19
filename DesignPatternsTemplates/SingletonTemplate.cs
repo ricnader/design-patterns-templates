f3bede6eee3b6511d232bbd0ce949b73cef3ff4e
ric.nader@gmail.com <ric.nader@gmail.com>
2023-07-19 09:53:52 -0300

    Repo creation containing a singleton template

---
+++ DesignPatternsTemplates/SingletonTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatternsTemplates
+{
+    public class SingletonTemplate
+    {
+        public static SingletonTemplate instance { get; private set; }
+        public string Value1 { get; set; }         
+
+        private SingletonTemplate()
+        {}
+
+        public void setValue1(string value) => this.Value1 = value;
+
+        public SingletonTemplate getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SingletonTemplate();
+            }
+            return instance;
+        }
+    }
+}
---
+++ SingletonTemplate.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesignPatternsTemplates;
+
+
+var singletonObject1 = SingletonTemplate.instance;
+var singletonObject2 = SingletonTemplate.instance;
+
+//singletonObject.setValue1("Teste");
+
+Console.WriteLine(singletonObject1 == singletonObject2);
+Console.ReadLine();
